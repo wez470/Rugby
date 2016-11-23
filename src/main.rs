84bb1b0573d7b04f15ebd326f462d86f8214ea96
Weston Carlson <wez470@gmail.com>
2016-11-22 20:22:17 -0600

    Add cpu registers.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 struct CPU {
+    af: u16,
+    bc: u16,
+    de: u16,
+    hl: u16,
+    sp: u16,
+    pc: u16
 }
 
 impl CPU {
     fn new() -> CPU {
         CPU {
+            af: 0,
+            bc: 0,
+            de: 0,
+            hl: 0,
+            sp: 0,
+            pc: 0
         }
     }
 }
