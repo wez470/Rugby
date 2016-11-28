38c10f3a933c3598e5f0264c96421c11cebb42d2
Weston Carlson <wez470@gmail.com>
2016-11-27 21:08:32 -0600

    Move memory out of cpu.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,28 +1,31 @@
 use reg_16::Reg16;
+use memory::Memory;
 
-pub struct Cpu {
+pub struct Cpu<'a> {
     reg_af: Reg16,
     reg_bc: Reg16,
     reg_de: Reg16,
     reg_hl: Reg16,
     reg_sp: Reg16,
     reg_pc: Reg16,
-    memory: Box<[u8; 0x10000]>,
+    memory: &'a mut Memory,
     rom: Box<[u8]>,
 }
 
-impl Cpu {
-    pub fn new(rom: Box<[u8]>) -> Cpu {
-        Cpu {
+impl<'a> Cpu<'a> {
+    pub fn new(rom: Box<[u8]>, mem: &mut Memory) -> Cpu {
+        let mut cpu = Cpu {
             reg_af: Reg16::new(),
             reg_bc: Reg16::new(),
             reg_de: Reg16::new(),
             reg_hl: Reg16::new(),
             reg_sp: Reg16::new(),
             reg_pc: Reg16::new(),
-            memory: Box::new([0; 0x10000]),
+            memory: mem,
             rom: rom,
-        }
+        };
+        cpu.reset();
+        cpu
     }
 
     pub fn reset(&mut self) {
@@ -33,46 +36,11 @@ impl Cpu {
         self.reg_de.set(0x00D8);
         self.reg_hl.set(0x014D);
         self.reg_sp.set(0xFFFE);
-        self.init_memory();
-    }
-
-    fn init_memory(&mut self) {
-        self.memory[0xFF05] = 0x00;
-        self.memory[0xFF06] = 0x00;
-        self.memory[0xFF07] = 0x00;
-        self.memory[0xFF10] = 0x80;
-        self.memory[0xFF11] = 0xBF;
-        self.memory[0xFF12] = 0xF3;
-        self.memory[0xFF14] = 0xBF;
-        self.memory[0xFF16] = 0x3F;
-        self.memory[0xFF17] = 0x00;
-        self.memory[0xFF19] = 0xBF;
-        self.memory[0xFF1A] = 0x7F;
-        self.memory[0xFF1B] = 0xFF;
-        self.memory[0xFF1C] = 0x9F;
-        self.memory[0xFF1E] = 0xBF;
-        self.memory[0xFF20] = 0xFF;
-        self.memory[0xFF21] = 0x00;
-        self.memory[0xFF22] = 0x00;
-        self.memory[0xFF23] = 0xBF;
-        self.memory[0xFF24] = 0x77;
-        self.memory[0xFF25] = 0xF3;
-        self.memory[0xFF26] = 0xF1;
-        self.memory[0xFF40] = 0x91;
-        self.memory[0xFF42] = 0x00;
-        self.memory[0xFF43] = 0x00;
-        self.memory[0xFF45] = 0x00;
-        self.memory[0xFF47] = 0xFC;
-        self.memory[0xFF48] = 0xFF;
-        self.memory[0xFF49] = 0xFF;
-        self.memory[0xFF4A] = 0x00;
-        self.memory[0xFF4B] = 0x00;
-        self.memory[0xFFFF] = 0x00;
     }
 
     pub fn run(&mut self) {
         let instruction = self.rom[self.reg_pc.get() as usize];
-        println!("{}", format!("{:02X}", instruction));
+        println!("{:02X}", instruction);
         self.reg_pc.inc();
         //fetchOpcode
         //executeOpcode
@@ -80,4 +48,6 @@ impl Cpu {
         //update graphics
         //Interrupts
     }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+mod memory;
 mod cpu;
 mod reg_16;
 
+use memory::Memory;
 use cpu::Cpu;
 use std::env;
 use std::fs::File;
@@ -13,7 +15,8 @@ fn main() {
     file.read_to_end(&mut file_buf).unwrap();
     let rom = file_buf.into_boxed_slice();
 
-    let mut cpu = Cpu::new(rom);
+    let mut mem = Memory::new();
+    let mut cpu = Cpu::new(rom, &mut mem);
     cpu.reset();
     for _ in 0..10 {
         cpu.run();
