0b85c3b413461e976984f56771b143988a4dd79c
Scott Olson <scott@solson.me>
2017-02-14 21:23:50 -0800

    Fix opcode for STOP.

--- src/cpu.rs
+++ src/cpu.rs
@@ -592,14 +592,14 @@ fn decode(bytes: &[u8]) -> Option<Inst> {
 
     let inst = match bytes[0] {
         0x00 => Nop,
-        0x01 => {
+        0x06 => Ld(Reg8(B), Imm8(bytes[1])),
+        0x10 => {
             // FIXME: For some reason the STOP instruction is followed by 0x00 according to the
             // manual. Perhaps this should result in an invalid instruction if it's not zero. For
             // now we'll assert, so it's obvious if we ever encounter this case.
             assert_eq!(bytes[1], 0);
             Stop
         }
-        0x06 => Ld(Reg8(B), Imm8(bytes[1])),
         0x11 => Ld(Reg16(DE), Imm16(to_u16(bytes[1], bytes[2]))),
         0x18 => Jr(bytes[1] as i8, Cond::None),
         0x28 => Jr(bytes[1] as i8, Cond::Zero),
