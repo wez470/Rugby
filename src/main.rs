6250fdb77a2f74aa9de3ce7e4f068a523d830217
Scott Olson <scott@solson.me>
2017-04-25 18:59:57 -0700

    Add a stub SDL2/glium window.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #[macro_use]
 extern crate bitflags;
 extern crate clap;
+#[macro_use]
+extern crate glium;
+extern crate glium_sdl2;
 extern crate rand;
+extern crate sdl2;
 
 use clap::{Arg, App, AppSettings, SubCommand};
 use cpu::Cpu;
@@ -31,6 +35,7 @@ fn main() {
             .arg(Arg::with_name("ROM")
                 .required(true)
                 .help("The game rom")))
+        .subcommand(SubCommand::with_name("window"))
         .get_matches();
 
     match app_matches.subcommand() {
@@ -60,6 +65,10 @@ fn main() {
             println!("{:#?}", cart_header);
         }
 
+        ("window", Some(_)) => {
+            open_window();
+        }
+
         _ => unreachable!(),
     }
 }
@@ -76,4 +85,38 @@ fn check_error<T, E: Display>(res: Result<T, E>, message: &'static str) -> T {
         println!("{}: {}", message, e);
         exit(1);
     })
+}
+
+fn open_window() {
+    use glium_sdl2::DisplayBuild;
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let display = video_subsystem.window("My window", 800, 600)
+        .resizable()
+        .build_glium()
+        .unwrap();
+
+    let mut running = true;
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    while running {
+        let mut target = display.draw();
+        // do drawing here...
+        target.finish().unwrap();
+
+        // Event loop: polls for events sent to all windows
+
+        for event in event_pump.poll_iter() {
+            use sdl2::event::Event;
+
+            match event {
+                Event::Quit { .. } => {
+                    running = false;
+                },
+                _ => ()
+            }
+        }
+    }
 }
