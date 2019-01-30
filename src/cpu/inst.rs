1aad076c72e2a47556b11f12148d4a0088ce34f6
Scott Olson <scott@solson.me>
2019-01-29 19:37:20 -0600

    Fix incorrect instruction lengths.

--- src/cpu/inst.rs
+++ src/cpu/inst.rs
@@ -72,11 +72,13 @@ pub const PREFIX_CB_BASE_CYCLES: [usize; 0x100] = [
      8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8, // E
      8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8, // F
 ];
-
 /// The length of every instruction. (1 byte for the opcode plus optional bytes for operands.)
 ///
 /// Every `0xCB`-prefixed instruction has length 2 (counting the `CB` opcode), so we handle that
 /// simply by putting a 2 in the 0xCB location.
+///
+/// NOTE: The original reference we used claimed 0xE2 and 0xF2 had length 2, but empirically they
+/// seem to be length 1.
 pub const INSTRUCTION_LENGTH: [usize; 0x100] = [
 //   0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  A,  B,  C,  D,  E,  F
      1,  3,  1,  1,  1,  1,  2,  1,  3,  1,  1,  1,  1,  1,  2,  1, // 0
@@ -93,8 +95,8 @@ pub const INSTRUCTION_LENGTH: [usize; 0x100] = [
      1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, // B
      1,  1,  3,  3,  3,  1,  2,  1,  1,  1,  3,  2,  3,  3,  2,  1, // C
      1,  1,  3,  1,  3,  1,  2,  1,  1,  1,  3,  1,  3,  1,  2,  1, // D
-     2,  1,  2,  1,  1,  1,  2,  1,  2,  1,  3,  1,  1,  1,  2,  1, // E
-     2,  1,  2,  1,  1,  1,  2,  1,  2,  1,  3,  1,  1,  1,  2,  1, // F
+     2,  1,  1,  1,  1,  1,  2,  1,  2,  1,  3,  1,  1,  1,  2,  1, // E
+     2,  1,  1,  1,  1,  1,  2,  1,  2,  1,  3,  1,  1,  1,  2,  1, // F
 ];
 
 /// The largest value in the `INSTRUCTION_LENGTH` table.
